934a6acaa4a0a64b43d0f73f241611621817b530
chankyupark <ckparks@gmail.com>
2022-10-24 16:05:22 +0900

    Update resnet50.py

--- resnet50.py
+++ resnet50.py
@@ -26,8 +26,53 @@ class ChannelAttn(nn.Module):
         x = self.conv2(x)
         return torch.sigmoid(x)
 
+class SpatialTransformBlock(nn.Module):
+    def __init__(self, num_classes, pooling_size, channels):
+        super(SpatialTransformBlock, self).__init__()
+        self.num_classes = num_classes
+        self.spatial = pooling_size
 
+        self.global_pool = nn.AvgPool2d((pooling_size, pooling_size//2), stride=1, padding=0, ceil_mode=True, count_include_pad=True)
 
+        self.gap_list = nn.ModuleList()
+        self.fc_list = nn.ModuleList()
+        self.att_list = nn.ModuleList()
+        self.stn_list = nn.ModuleList()
+        for i in range(self.num_classes):
+            self.gap_list.append(nn.AvgPool2d((pooling_size, pooling_size//2), stride=1, padding=0, ceil_mode=True, count_include_pad=True))
+            self.fc_list.append(nn.Linear(channels, 1))
+            self.att_list.append(ChannelAttn(channels))
+            self.stn_list.append(nn.Linear(channels, 4))
+
+    def stn(self, x, theta):
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid, padding_mode='border', align_corners=True)
+        return x.cuda()
+
+    def transform_theta(self, theta_i, region_idx):
+        theta = torch.zeros(theta_i.size(0), 2, 3)
+        theta[:,0,0] = torch.sigmoid(theta_i[:,0])
+        theta[:,1,1] = torch.sigmoid(theta_i[:,1])
+        theta[:,0,2] = torch.tanh(theta_i[:,2])
+        theta[:,1,2] = torch.tanh(theta_i[:,3])
+        theta = theta.cuda()
+        return theta
+
+    def forward(self, features):
+        pred_list = []
+        bs = features.size(0)
+        for i in range(self.num_classes):
+            stn_feature = features * self.att_list[i](features) + features
+
+            theta_i = self.stn_list[i](F.avg_pool2d(stn_feature, stn_feature.size()[2:]).view(bs,-1)).view(-1,4)
+            theta_i = self.transform_theta(theta_i, i)
+
+            sub_feature = self.stn(stn_feature, theta_i)
+            pred = self.gap_list[i](sub_feature).view(bs,-1)
+            pred = self.fc_list[i](pred)
+            pred_list.append(pred)
+        pred = torch.cat(pred_list, 1)
+        return pred
 
 class Reshape(nn.Module):
     def __init__(self, *args):
@@ -37,7 +82,6 @@ class Reshape(nn.Module):
     def forward(self, x):
         return x.view(self.shape)
 
-
 class Resnet50(nn.Module):
     def __init__(self, num_classes=13, attribute_dim=None):
         super(Resnet50, self).__init__()
@@ -46,7 +90,9 @@ class Resnet50(nn.Module):
         self.global_pool = nn.AvgPool2d((8,4), stride=1, padding=0, ceil_mode=True, count_include_pad=True)
         self.finalfc = nn.Linear(2048, num_classes)
 
-        
+        self.st_2 = SpatialTransformBlock(num_classes, 32, 256*3)
+        self.st_3 = SpatialTransformBlock(num_classes, 16, 256*2)
+        self.st_4 = SpatialTransformBlock(num_classes, 8,  256*1)
         # Lateral layers
 
         self.latlayer_1 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
